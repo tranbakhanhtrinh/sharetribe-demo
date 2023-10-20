3f0bdb51bcf2a47cdd82aa31ded76a11894f70ad
tranbakhanhtrinh <tranbakhanhtrinh@gmail.com>
2023-10-20 17:48:21 +0700

    first commmit

---
+++ src/components/Reviews/Reviews.js
@@ -0,0 +1,108 @@
+import React from 'react';
+import { injectIntl, intlShape } from '../../util/reactIntl';
+import { arrayOf, string } from 'prop-types';
+import classNames from 'classnames';
+import { Avatar, ReviewRating, UserDisplayName } from '../../components';
+import { propTypes } from '../../util/types';
+
+import css from './Reviews.module.css';
+import { array } from 'prop-types';
+
+const Review = props => {
+  const { review, intl } = props;
+
+  const date = review.attributes.createdAt;
+  const dateString = intl.formatDate(date, { month: 'long', year: 'numeric' });
+
+  return (
+    <div className={css.review}>
+      <Avatar className={css.avatar} user={review.author} />
+      <div>
+        <ReviewRating
+          rating={review.attributes.rating}
+          className={css.mobileReviewRating}
+          reviewStarClassName={css.reviewRatingStar}
+        />
+        <p className={css.reviewContent}>{review.attributes.content}</p>
+        <p className={css.reviewInfo}>
+          <UserDisplayName user={review.author} intl={intl} />
+          <span className={css.separator}>•</span>
+          {dateString}
+          <span className={css.desktopSeparator}>•</span>
+          <span className={css.desktopReviewRatingWrapper}>
+            <ReviewRating
+              rating={review.attributes.rating}
+              className={css.desktopReviewRating}
+              reviewStarClassName={css.reviewRatingStar}
+            />
+          </span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+Review.propTypes = {
+  review: propTypes.review.isRequired,
+  intl: intlShape.isRequired,
+};
+
+export const HobbiesOfReviewsPage = props => {
+  const { hobbies } = props
+  const styles = {
+    listStyle: 'disc',
+    textTransform: 'capitalize'
+  }
+  return (
+    <ul style={styles}>
+      {hobbies.map((r, index) => (
+        <li key={`Hobby_${r}_${index}`}>
+          {r}
+        </li>
+      ))}
+    </ul>
+  )
+}
+
+HobbiesOfReviewsPage.defaultProps = {
+  hobbies: []
+}
+HobbiesOfReviewsPage.propTypes = {
+  hobbies: array
+}
+
+const ReviewsComponent = props => {
+  const { className, rootClassName, reviews, intl } = props;
+  const classes = classNames(rootClassName || css.root, className);
+
+  return (
+    <ul className={classes}>
+      {reviews.map(r => {
+        return (
+          <li key={`Review_${r.id.uuid}`} className={css.reviewItem}>
+            <Review review={r} intl={intl} />
+          </li>
+        );
+      })}
+    </ul>
+  );
+};
+
+ReviewsComponent.defaultProps = {
+  className: null,
+  rootClassName: null,
+  reviews: [],
+};
+
+ReviewsComponent.propTypes = {
+  className: string,
+  rootCalssName: string,
+  reviews: arrayOf(propTypes.review),
+
+  // from injectIntl
+  intl: intlShape.isRequired,
+};
+
+const Reviews = injectIntl(ReviewsComponent);
+
+export default Reviews;
